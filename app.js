a4b9fb8aaf6c5a1355ca0783fb9430f22410e43a
Julien Dané <julien.dane@gmail.com>
2023-03-03 12:36:52 +0100

    use morgan for logging

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
-const routes = require("./routes");
 const bodyParser = require("body-parser");
+const logger = require("morgan");
+
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(logger("dev"));
 
 app.use("/api", routes);
 
