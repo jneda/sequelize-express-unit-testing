7f7bb700c8a5407b51a4db615b941e54038fbbf1
Julien Dané <julien.dane@gmail.com>
2023-03-03 11:03:27 +0100

    basic server running

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+const express = require("express");
+const routes = require("./routes");
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use("/api", routes);
+
+try {
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+} catch (err) {
+  console.err(err);
+  process.exit(1);
+}
